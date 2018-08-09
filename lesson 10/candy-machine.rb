8b972d6f49ba7da2e7c98b8726cacb0fa973a2b5
Jansen Martin <v.jansen.martin@gmail.com>
2018-08-09 15:45:47 -0500

    Add changes to reflect JSL Day 1-2 lessons, and define methods for printing messages and calculating change

--- lesson 10/candy-machine.rb
+++ lesson 10/candy-machine.rb
@@ -1,3 +1,4 @@
+# Sets prices for goods
 twix = 0.65
 chips = 0.50
 nutter_butter = 0.75
@@ -6,80 +7,95 @@ gum = 0.55
 allowance = 0 
 change = 0
 
-puts "Welcome to my virtual vending machine!"
-
-# do loop for collecting user's starting dollar amount
-# allowance variable is assigned to user input
-# .to_f converts user input to a float.  "abc".to_f returns 0
-#  if allowance equals 0, then user is prompted to enter info again
-
-loop do
-  print "\nHow much money ya got?"
-  allowance = gets.chomp.to_f 
-  if allowance == 0
-  puts "\nInvalid entry.  Please try again."
-  next
-  else
-    # '%.2f' % allowance displays trailing zeros in value assigned to "allowance"
-    puts "Just $#{'%.2f' % allowance}, huh?  Okay, then..."
-  break
-  end
+# Calculates change based on user's item selection and current dollar amount
+def change(allowance, item)
+  change = allowance - item
+  puts "\nHere you go!  Your change is $#{'%.2f' % change}."
 end
 
-# List of pre-determined vending machine items and prices
-puts "\nHere's a list of our wares!"
+# Prints message if user doesn't have enough money to purchase selected item
+def broke(allowance)
+  print "\nYou're broke!  Take your $#{'%.2f' % allowance} and go somewhere else!"
+end
 
+# Prints message to the user
+puts "Welcome to my virtual vending machine!"
+
+# Collects user's starting dollar amount
+loop do
+  print "\nHow much money ya got? "
+  allowance = gets.chomp.to_f
+
+# Prints message based on user's input
+  if allowance == 0
+    puts "\nInvalid entry.  Please try again: "
+    next
+  else
+    puts "\nJust $#{'%.2f' % allowance}, huh?  Okay, then..."
+    break
+  end
+
+end
+
+# Prints pre-determined vending machine items and prices
+puts "\nHere's a list of our tasty treats!"
 puts "\nA. $#{twix} Twix
 B. $#{'%.2f' % chips} Chips
 C. $#{nutter_butter} Nutter Butters
 D. $#{peanut_butter_cup} Peanut Butter Cup
 E. $#{gum} Juicy Fruit Gum"
 
-# Do loop that collects user's item selection, 
-# checks if the user has enough money, and calculates change.
-# Loop starts over if user enters invalid selection.
+# Prints message to user and collects their item section
 loop do
-print "\nSo, what'll it be? > "
-# .downcase converts user input into lowercase lettering
-letter = gets.chomp.downcase
+  print "\nSo, what'll it be? > "
+  letter = gets.chomp.downcase
 
-if letter == "a" || letter == "d"
-  if allowance >= twix || allowance >= peanut_butter_cup
-  change = allowance - twix
-  puts "\nHere you go!  Your change is $#{'%.2f' % change}."
-  else
-  print "You're broke!  Take your $#{'%.2f' % allowance} and go somewhere else!"
-  end
-  
-elsif letter == "b"
-  if allowance >= chips
-    change = allowance - chips
-    print "\nHere you go!  Your change is $#{'%.2f' % change}."
-  else 
-    print "You're broke!  Take your $#{'%.2f' % allowance} and go somewhere else!"
-  end
+  # Prints message based on user selection and starting dollar amount
+  # Calculates change for user if allowance is greater than or equal to item's price
+  if letter == "a"
 
-elsif letter == "c"
-  if allowance >= nutter_butter
-    change = allowance - nutter_butter
-    puts "\nHere you go!  Your change is $#{'%.2f' % change}."
+    if allowance >= twix
+      change(allowance, twix)
+    else
+      broke(allowance)
+    end
+
+  elsif letter == "b"
+
+    if allowance >= chips
+      change(allowance, chips)
+    else
+      broke(allowance) 
+    end
+
+  elsif letter == "c"
+
+    if allowance >= nutter_butter
+      change(allowance, nutter_butter)   
+    else
+      broke(allowance)
+    end
+
+  elsif letter == "d"
+
+    if allowance >= peanut_butter_cup
+      change(allowance, peanut_butter_cup)
+    else
+      broke(allowance)
+    end
+    
+  elsif letter == "e"
+
+    if allowance >= gum
+      change(allowance, gum)
+    else
+      broke(allowance)
+    end
     
   else
-    print "You're broke!  Take your $#{'%.2f' % allowance} and go somewhere else!"
+    puts "\nInvalid selection!"
+    next
   end
-  
-elsif letter == "e"
-  if allowance >= gum
-    change = allowance - gum
-    puts "\nHere you go!  Your change is $#{'%.2f' % change}."
-    
-  else
-    print "You're broke!  Take your $#{'%.2f' % allowance} and go somewhere else!"
-  end
-  
-else
-puts "\nInvalid selection!"
-next
-end
-break
+
+  break
 end
